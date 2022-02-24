828e1ed32dc67ad1a218f4e1972be6dd5631d23c
haemin-lee <haeminle@usc.edu>
2022-02-23 21:10:42 -0800

    D

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 import requests
+import RPi.GPIO as GPIO
 
 url = 'https://chordhero-backend.herokuapp.com/'
 
-x = requests.get(url)
 
-print("hello please work")
-print(x.text)
+ledPin = 21 
+buttonPin = 20
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while(1):
+    if GPIO.input(buttonPin): #button is pressed
+        GPIO.output(ledPin, GPIO.HIGH)
+        x = requests.get(url)
+        print(x.text)
+    else:
+        GPIO.output(ledPin, GPIO.HIGH)
