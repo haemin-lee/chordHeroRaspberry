1dfe2e4777eae36548fd8d1cc4adeac0cc114f69
haemin-lee <haeminle@usc.edu>
2022-02-25 20:53:33 -0800

    yes

--- add.py
+++ add.py
@@ -1,12 +1,17 @@
 import requests
 
 url = 'http://localhost:5000/uploadNewSong'
-url2 = 'https://chordhero-backend.herokuapp.com/'
-myobj = {'twinkletwinkle': [['C', 'D', 'F'], ['D', 'E', 'G'], ['C', 'F', 'A']]}
+url2 = 'http://localhost:5000/getSong'
+myobj1 = {'name': 'twinkletwinkle', 'keys': [['C', 'D', 'F'], ['D', 'E', 'G'], ['C', 'F', 'A']]}
+myobj2 = {'name': 'silentnight', 'keys': [['C', 'D', 'F#'], ['D', 'E#', 'G'], ['C#', 'F', 'A']]}
+myobj3 = {'name': 'jinglebells', 'keys': [['C', 'A', 'D'], ['D', 'F', 'B'], ['C', 'A', 'A#']]}
 
-x = requests.post(url, data = myobj)
+obj = {'id': '0'}
 
-y = requests.get(url2, data = myobj)
+x = requests.post(url, json = myobj1)
+y = requests.post(url, json = myobj2)
+z = requests.post(url, json = myobj3)
 
-print(x.text)
-print(y.text)
+a  = requests.post(url2, json=obj)
+
+
