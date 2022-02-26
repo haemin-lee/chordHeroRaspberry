94f529fd2cf2d7d924d6b811ba9f754a929238f5
haemin-lee <haeminle@usc.edu>
2022-02-25 20:30:37 -0800

    D

--- add.py
+++ add.py
@@ -1,8 +1,12 @@
 import requests
 
-url = 'https://chordhero-backend.herokuapp.com/'
+url = 'http://localhost:5000/uploadNewSong'
+url2 = 'https://chordhero-backend.herokuapp.com/'
 myobj = {'twinkletwinkle': [['C', 'D', 'F'], ['D', 'E', 'G'], ['C', 'F', 'A']]}
 
 x = requests.post(url, data = myobj)
 
+y = requests.get(url2, data = myobj)
+
 print(x.text)
+print(y.text)
---
+++ camitest.py
@@ -0,0 +1,20 @@
+import requests
+import RPi.GPIO as GPIO
+
+url = 'https://chordhero-backend.herokuapp.com/'
+
+
+ledPin = 21 
+buttonPin = 20
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while(1):
+    if GPIO.input(buttonPin): #button is pressed
+        GPIO.output(ledPin, GPIO.LOW)
+    else:
+        GPIO.output(ledPin, GPIO.HIGH)
+        x = requests.get(url)
+        print(x.text)
+        
